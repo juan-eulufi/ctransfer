f2cb2a27715073227878a8c4e75af07e17a98730
juan.eulufi <juan.eulufi.sa@gmail.com>
2025-10-28 12:46:23 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,110 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { supabase } from "@/lib/supabaseClient";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { CheckCircle2Icon } from "lucide-react";
+import { Label } from "@radix-ui/react-label";
+
+import { IconCloud } from "@tabler/icons-react"
+
+
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty"
+
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [file, setFile] = useState<File | null>(null);
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0] || null;
+    setFile(selectedFile);
+  };
+
+  const uploadFile = async () => {
+    if (!file) {
+      alert("Selecciona un archivo primero");
+      return;
+    }
+
+    // Guardamos con un nombre único
+    const fileName = `${Date.now()}_${file.name}`;
+
+    const { data, error } = await supabase.storage
+      .from("files") // Reemplaza con el nombre de tu bucket
+      .upload(fileName, file);
+
+    if (error) {
+      console.error("Error subiendo archivo:", error);
+      return;
+    }
+
+    // Obtener URL pública
+    const { data: publicUrlData } = supabase.storage
+      .from("files")
+      .getPublicUrl(fileName);
+
+    setFileUrl(publicUrlData.publicUrl);
+  };
+
+  return (
+    <div><nav className="bg-gray-700 fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span className="scroll-m-20 text-white text-center text-4xl font-extrabold tracking-tight text-balance">Ctransfer</span>
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+         
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+    </nav>
+      <div className="font-sans bg-indigo-50 grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+        <main className="flex text-gray-800 flex-col  gap-[32px] row-start-2  sm:items-start">
+          <ol className="scroll-m-20 border-b pb-2 text-4xl font-semibold tracking-tight first:mt-0">
+            Ctransfer
+          </ol>
+
+          <div className="flex gap-4 items-start flex-col">
+            <Label htmlFor="files" className="scroll-m-20 text-2xl font-semibold tracking-tight">Comparte con tu grupo de amigos o trabajo. 👋</Label>
+            <p className="text-xs text-gray">Archivos de los formatos más utilizados, como PDF, PNG, JPG, Word, Excel, ZIP, entre otros.</p>
+            <Input className="text-xs hover:bg-gray-100 hover:cursor-pointer" id="files" type="file" onChange={handleFileChange} />
+          </div>
+
+          <Button
+            className="text-black text-xs hover:border-gray-300 hover:bg-gray-100 cursor-pointer"
+            type="button"
+            variant="outline"
+            onClick={uploadFile}
+          >
+            Compartir archivo 🔗
+          </Button>
+
+
+          {fileUrl && (
+            <Alert className="mt-10">
+              <CheckCircle2Icon />
+              <AlertTitle>Success! Your file in:</AlertTitle>
+              <AlertDescription>
+                <a href={fileUrl} target="_blank" className="underline hover:text-blue-300" rel="noopener noreferrer">
+                  {fileUrl}
+                </a>
+              </AlertDescription>
+            </Alert>
+
+
+          )}
+
+        </main>
+      </div>
     </div>
   );
 }
